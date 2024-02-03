98c2fc2672121d06d6d3786daee1f594509002b5
Carliane <carllylim@gmail.com>
2024-02-03 15:46:31 -0300

    Adding the modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
 
-const maxRecords = 151
-const limit = 10
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
+        <li class="pokemon ${pokemon.type}" onclick="abrirModal('vis-modal', this)">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
@@ -16,8 +16,7 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `
@@ -45,3 +44,41 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+//----- modal informartions -------
+function abrirModal(abrirModal, clickedPokemon) {
+    let modal = document.getElementById(abrirModal);
+
+    modal.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+
+    // Encontrar o elemento .name dentro do elemento clicado
+    let pokemonName = clickedPokemon.querySelector('.name').innerText;
+    console.log('pokemonName: ' + pokemonName);
+
+    document.getElementById('pokemonName').innerText = pokemonName;
+
+    //image
+    let pokemonImage = clickedPokemon.querySelector('img').src;
+    document.getElementById('pokemonImage').src = pokemonImage;
+
+    //number
+    let pokemonNumber = clickedPokemon.querySelector('.number').innerText;
+    document.getElementById('pokemonNumber').innerText = pokemonNumber;
+
+    //types
+    let pokemonTypes = clickedPokemon.querySelectorAll('.type');
+    let types = '';
+    pokemonTypes.forEach(type => {
+        types += type.innerText + ' ';
+    });
+    document.getElementById('pokemonTypes').innerText = types;
+    
+}
+
+function fecharModal(fecharModal) {
+    let modal = document.getElementById(fecharModal);
+
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
